a44160f882e2829c1524488726413d895747f974
Oscar Cordoba <ocordoba3@hotmail.com>
2025-10-14 11:44:53 -0500

    css file removed

--- src/app/shared/components/country-list/country-list.ts
+++ src/app/shared/components/country-list/country-list.ts
@@ -17,7 +17,6 @@ import { Subscription } from 'rxjs';
   selector: 'app-country-list',
   imports: [],
   templateUrl: './country-list.html',
-  styleUrl: './country-list.css',
 })
 export class CountryList implements OnDestroy {
   activatedRoute = inject(ActivatedRoute);
